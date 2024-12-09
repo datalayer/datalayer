d1680899176fd5657b6f06c11132e5bef71ebb77
Eric Charles <eric@datalayer.io>
2024-12-09 11:36:03 +0100

    Conda package (#6)
    * docs: lint
    
    * fix: conda biuld
    
    * build: conda

---
+++ conda-recipe/post-link.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# THIS SCRIPT IS A WORKAROUND BECAUSE THE DEPENDENCIES ARE NOT AVAILABLE IN ANY CONDA CHANNEL.
+
+"${PREFIX}/bin/pip" install datalayer_core==1.0.23 jupyter_iam==1.2.6 jupyter_kernels==1.2.13
