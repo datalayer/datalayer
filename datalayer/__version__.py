5b31ac12b74c4e4405ecc9a0b7f563dcbd3aa944
Eric Charles <eric@datalayer.io>
2025-02-26 10:01:14 +0100

    dep: jupyterlab (#8)
    * dep: jupyterlab
    
    * Automatic application of license header
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- datalayer/__version__.py
+++ datalayer/__version__.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+# Distributed under the terms of the Modified BSD License.
+
 # TODO Fix me as duplicate with _version.py
 
-VERSION = "1.2.19"
+VERSION = "1.2.20"
--- hatch_build.py
+++ hatch_build.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2023-2024 Datalayer, Inc.
+# Distributed under the terms of the Modified BSD License.
+
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class JupyterBuildHook(BuildHookInterface):
