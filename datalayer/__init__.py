9ed6dc00d155d4f6f1b79d617218f86c63aabb20
Eric Charles <eric@datalayer.io>
2025-03-01 19:12:34 +0100

    conda

--- datalayer/__init__.py
+++ datalayer/__init__.py
@@ -1,12 +1,2 @@
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
-
-def _jupyter_labextension_paths():
-    return []
-
-
-def _jupyter_server_extension_points():
-    return []
-
-def _load_jupyter_server_extension(server_app):
-    pass
--- hatch_build.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023-2024 Datalayer, Inc.
-# Distributed under the terms of the Modified BSD License.
-
-from hatchling.builders.hooks.plugin.interface import BuildHookInterface
-
-class JupyterBuildHook(BuildHookInterface):
-    def initialize(self, version, build_data):
-        pass
