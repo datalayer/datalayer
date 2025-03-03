f295ea6a764da4403427b3b34487ddbe46eaa111
Eric Charles <eric@datalayer.io>
2025-03-03 10:18:21 +0100

    Rel/1.3.0 (#9)
    * bump: 1.3.0
    
    * deps: jupyter kernels
    
    * bump: version
    
    * conda
    
    * conda: build
    
    * Automatic application of license header
    
    * bump: version
    
    * bump: version
    
    * Automatic application of license header
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- conda-recipe/post-link.sh
+++ conda-recipe/post-link.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
 
+# THIS INSTALL IS A WORKAROUND.
 
-# THIS SCRIPT IS A WORKAROUND BECAUSE THE DEPENDENCIES ARE NOT AVAILABLE IN ANY CONDA CHANNEL.
-
-"${PREFIX}/bin/pip" install datalayer_core==1.0.23 jupyter_iam==1.2.6 jupyter_kernels==1.2.13
+"${PREFIX}/bin/pip" install datalayer_core datalayer_ui jupyter_iam jupyter_kernels
--- conda-recipe/publish-conda.sh
+++ conda-recipe/publish-conda.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
 
-
-export DATALAYER_VERSION=1.2.14
+export DATALAYER_VERSION=1.3.1
 
 CONDA_CHANNEL_NAME=datalayer
+
 ORGANIZATION=datalayer
 
 # Make sure the script stops on any error (errors are otherwise hard to spot)
@@ -13,7 +14,7 @@ set -o errtrace -o nounset -o pipefail -o errexit
 
 rm -fr ./out
 
-PKG_PATH=$(conda-build --output-folder ./out . -c defaults)
+PKG_PATH=$(conda-build --output-folder ./out . -c defaults -c echarles -c datalayer)
 
 echo $PKG_PATH generated
 
