4d0d5396d3674b4c17ee69f4e441a409014acc9f
Eric Charles <eric@datalayer.io>
2025-03-24 10:29:08 +0100

    chore: add extension point

--- datalayer/__init__.py
+++ datalayer/__init__.py
@@ -1,2 +1,7 @@
 # Copyright (c) 2023-2024 Datalayer, Inc.
 # Distributed under the terms of the Modified BSD License.
+
+from typing import Any, Dict, List
+
+def _jupyter_server_extension_points() -> List[Dict[str, Any]]:
+    return []
